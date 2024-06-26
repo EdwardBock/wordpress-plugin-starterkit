019515178a80b601bd6bb176a63d1ab4b8ef2065
Edward <edward.bock@palasthotel.de>
2024-06-26 22:06:51 +0200

    instructions on public functions

--- plugin/public-functions.php
+++ plugin/public-functions.php
@@ -1,3 +1,8 @@
 <?php
 
-// functions that can be used outside the plugin
+// public functions API
+// use typical wordpress namespace via prefix functions
+// do not use namespace
+function wordpress_plugin_starterkit_do_something(){
+
+}
